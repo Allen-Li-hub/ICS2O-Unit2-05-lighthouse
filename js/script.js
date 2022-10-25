aac735a71ca112a519d0ac4db4a33f82ba3b04e1
Allen-Li-hub <113055658+Allen-Li-hub@users.noreply.github.com>
2022-10-25 14:07:16 -0400

    change

--- js/script.js
+++ js/script.js
@@ -18,6 +18,16 @@ if (navigator.serviceWorker) {
 /**
  * This function displays an alert.
  */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+function calculate() {
+  const hours = parseInt(
+    document.getElementById("number-of-hours-worked").value
+  )
+  const rate = parseInt(document.getElementById("Hourly-rate:$").value)
+
+  const take = hours * rate * 0.18
+  const wage = hours * rate * (1.0 - 0.18)
+
+  document.getElementById("take").innerHTML =
+    "The governemnt will take: " + take + "$"
+  document.getElementById("wage").innerHTML = "your pay will be: " + wage + "$"
 }
